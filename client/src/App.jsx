fd5d72594072688ffa119232508a216f8863f678
Softzee18 <machaladora4@gmail.com>
2025-12-03 22:12:58 +0100

    Uncomment imports and fix AdminDashboard casing
    Uncommented the import statements and fixed the casing for AdminDashboard import.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,6 @@
-// import { Toaster } from "sonner";
-// import "./App.css";
-// import AdminDashboard from "./pages/adminDashboard";
-// import DoctorsDashboard from "./pages/DoctorsDashboard";
-// import LoginPage from "./pages/LoginPage";
-// import NotFound from "./pages/NotFound";
-// import NurseDashboard from "./pages/NurseDashboard";
-// import SigupPage from "./pages/SigupPage";
-// import UserDashboard from "./pages/UserDashboard";
-// import { Routes, Route, BrowserRouter } from "react-router";
-// import { ProtectedRoute } from "./ProtectedRoutes/ClientProtected";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Toaster position="top-right" richColors />
-//       <Routes>
-//         <Route path="*" element={<NotFound />} />
-//         <Route path="/auth/signup" element={<SigupPage />} />
-//         <Route path="/auth/login" element={<LoginPage />} />
-//         <Route element={<ProtectedRoute requiredRole="doctor" />}>
-//           <Route path="/doctor/dashboard" element={<DoctorsDashboard />} />
-//         </Route>
-//         <Route element={<ProtectedRoute requiredRole="admin" />}>
-//           <Route path="/admin/dashboard" element={<AdminDashboard />} />
-//         </Route>
-//         <Route element={<ProtectedRoute requiredRole="user" />}>
-//           <Route path="/user/dashboard" element={<UserDashboard />} />
-//         </Route>
-//         <Route element={<ProtectedRoute requiredRole="nurse" />}>
-//           <Route path="/nurse/dashboard" element={<NurseDashboard />} />
-//         </Route>
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
 import { Toaster } from "sonner";
 import "./App.css";
-import AdminDashboard from "./pages/adminDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
 import DoctorsDashboard from "./pages/DoctorsDashboard";
 import LoginPage from "./pages/LoginPage";
 import NotFound from "./pages/NotFound";
